6eb22c5399a0f2b85fbcc3f177420bcb30b6e1b6
Vladimir Kiselev <vladimir.yu.kiselev@gmail.com>
2016-04-15 13:58:50 +0000

    add new files
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SC3@116354 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ tests/testthat/test-de.R
@@ -0,0 +1,6 @@
+d <- SC3:::cell_filter(treutlein, 2000)
+d <- SC3:::gene_filter(d, 0.06)
+
+res <- get_de_genes(d, colnames(d))
+expect_is(res, "numeric")
+expect_equal(length(res), 87)
---
+++ tests/testthat/test-markers.R
@@ -0,0 +1,7 @@
+d <- SC3:::cell_filter(treutlein, 2000)
+d <- SC3:::gene_filter(d, 0.06)
+
+res <- get_marker_genes(d, colnames(d))
+expect_is(res, "data.frame")
+expect_equal(dim(res)[1], 95)
+expect_equal(dim(res)[2], 3)
